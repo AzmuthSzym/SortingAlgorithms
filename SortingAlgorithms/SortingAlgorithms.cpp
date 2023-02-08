8bf64e0c2fed59099219eca04965e85af09076c7
AzmuthSzym <simondziadek@gmail.com>
2023-02-08 22:16:35 +0100

    Adding simple console menu

--- SortingAlgorithms/SortingAlgorithms.cpp
+++ SortingAlgorithms/SortingAlgorithms.cpp
@@ -25,26 +25,49 @@ int main()
     int test_arr[] = { 6 , 4, 2, 7, 55, 12, 1 };
     int test_arr2[] = { 6 , 4, 2, 7, 55, 12, 1 };
     int n = sizeof(test_arr) / sizeof(test_arr[0]);
+    int choice;
+
     cout << "----------------------------------------" << endl;
     cout << "ARRAY BEFORE SORTING" << endl;
     printArray(test_arr);
     cout << "----------------------------------------\n" << endl;
 
-    cout << "INSERTION SORT" << endl;
-    cout << "----------------------------------------" << endl;
-    insertion_sort(test_arr, n);
-    printArray(test_arr);
-    cout << "----------------------------------------\n" << endl;
 
-    /*cout << "BUBBLE SORT" << endl;
-    cout << "----------------------------------------" << endl;
-    bubble_sort(test_arr, n);
-    printArray(test_arr);
-    cout << "----------------------------------------\n" << endl;
+    cout << "MAKE YOUR CHOICE" << endl;
+    cout << "0 - bubble, 1 - selection, 2 - insertion" << endl;
+    cin >> choice;
+    switch (choice)
+    {
+    case 0:
+        cout << "BUBBLE SORT" << endl;
+        cout << "----------------------------------------" << endl;
+        bubble_sort(test_arr, n);
+        printArray(test_arr);
+        cout << "----------------------------------------\n" << endl;
+        break;
+    case 1:
+        cout << "SELECTION SORT" << endl;
+        cout << "----------------------------------------" << endl;
+        selection_sort(test_arr2, n);
+        printArray(test_arr2);
+        cout << "----------------------------------------\n" << endl;
+        break;
+    case 2:
+        cout << "INSERTION SORT" << endl;
+        cout << "----------------------------------------" << endl;
+        insertion_sort(test_arr, n);
+        printArray(test_arr);
+        cout << "----------------------------------------\n" << endl;
+        break;
+    default:
+        cout << "YOU DIDNT CHOOSE VALID OPTION" << endl;
+        break;
+    }
 
-    cout << "SELECTION SORT" << endl;
-    cout << "----------------------------------------" << endl;
-    selection_sort(test_arr2, n);
-    printArray(test_arr2);
-    cout << "----------------------------------------\n" << endl;*/
+
+
+
+
+
+
 }
